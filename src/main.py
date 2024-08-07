caf1c1e78a0c01922a19cad072980705b0522704
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2024-08-07 16:39:49 +0300

    templates jinja

--- src/main.py
+++ src/main.py
@@ -3,16 +3,33 @@
 
 from logger import getLogger
 from flask import Flask, render_template, request, flash, redirect, jsonify
-from dotenv_vault import load_dotenv
 
 logger = getLogger()
 app = Flask(__name__)
-load_dotenv()
+
+
+@app.route("/")
+def index():
+    title = "Home"
+    return render_template("view.home.tmpl", title=title)
+
+
+@app.route("/about")
+def hello_world():
+    return "<h1>Hello, World!</h1>"
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('view.error.tmpl', error=error), 404
+
 
 if __name__ == "__main__":
-    logger.info("start app")
-    logger.debug("test debug")
-    logger.warning("test warning")
-    logger.error("test error")
-    logger.critical("test critical")
-    logger.info("finish app")
+    # logger.info("start app")
+    # logger.debug("test debug")
+    # logger.warning("test warning")
+    # logger.error("test error")
+    # logger.critical("test critical")
+    # logger.info("finish app")
+
+    app.run(debug=True)
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import 'assets/scss/app.scss'
-
-document
-    .querySelector<HTMLDivElement>('#app')!
-    .innerHTML = `
-<h1>Binance</h1>
-<div id="chart" style="height: 400px"></div>
-`
