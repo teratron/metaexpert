9c930f75f12a66350730d2440fd63d41ebdfdc38
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2024-08-07 11:27:24 +0300

    Add logger.py

--- src/logger.py
+++ src/logger.py
@@ -6,17 +6,14 @@ logger = getLogger(__name__)
 format_log = "%(asctime)s %(levelname)s: %(name)s: %(message)s"
 
 # File
-file_handler = FileHandler("data.log")
+file_handler = FileHandler("data.log", encoding="utf-8")
 file_handler.setLevel(WARNING)
 
 # Stream
 stream_handler = StreamHandler()
 stream_handler.setLevel(DEBUG)
 
-basicConfig(level=DEBUG, format=format_log, handlers=[file_handler, stream_handler], encoding="utf-8")
-
-# file_handler.close()
-# stream_handler.close()
+basicConfig(level=DEBUG, format=format_log, handlers=[file_handler, stream_handler])
 
 logger.info("start app")
 logger.debug("test debug")
@@ -24,3 +21,6 @@ logger.warning("test warning")
 logger.error("test error")
 logger.critical("test critical")
 logger.info("finish app")
+
+file_handler.close()
+stream_handler.close()
