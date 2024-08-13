79e391bd8633acdf7ae09bd52b879db45eb843c6
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2024-08-13 12:40:49 +0300

    infos

--- src/_binance.py
+++ src/_binance.py
@@ -6,7 +6,7 @@ import pandas
 from binance.spot import Spot
 from _logger import getLogger
 
-_log = getLogger(__name__)
+_logger = getLogger(__name__)
 
 
 class SpotAPI:
@@ -14,12 +14,18 @@ class SpotAPI:
 
     def __init__(self, api_key=None, api_secret=None, base_url=__default_url) -> None:
         if not SpotAPI.query_status():
-            _log.error("Binance is not available")
+            _logger.error("Binance is not available")
             sys.exit(1)
 
         self.api_key = api_key
         self.api_secret = api_secret
-        self.spot = Spot(api_key=api_key, api_secret=api_secret, base_url=base_url)
+        self.base_url = base_url
+
+        try:
+            self.spot = Spot(api_key=api_key, api_secret=api_secret, base_url=base_url)
+        except ConnectionError as error:
+            _logger.error(error)
+            sys.exit(1)
 
     # Проверка статуса Binance
     @staticmethod
@@ -27,10 +33,10 @@ class SpotAPI:
         try:
             status = Spot().system_status()
             if status["status"] == 0:
-                _log.info(f"System status {status['msg']}")
+                _logger.info(f"System status {status['msg']}")
                 return True
         except ConnectionError as error:
-            _log.error(error)
+            _logger.error(error)
         return False
 
     # Получение данных аккаунта с Binance
@@ -39,7 +45,7 @@ class SpotAPI:
 
     # Проверка состояния соединения с Binance
     def query_testnet(self) -> None:
-        _log.info(f"Check server time test {self.spot.time()['serverTime']}")
+        _logger.info(f"Check server time test {self.spot.time()['serverTime']}")
 
     # Получение списка биржевых пар
     def query_quote_asset_list(self, quote_asset_symbol: str) -> pandas.DataFrame:
@@ -49,34 +55,32 @@ class SpotAPI:
         quote_symbol_dataframe = quote_symbol_dataframe.loc[quote_symbol_dataframe["status"] == "TRADING"]
         return quote_symbol_dataframe
 
+    # Получение данных свечей по инструменту
+    def get_candlestick_data(self, symbol, timeframe, period) -> list:
+        raw_data = self.spot.klines(symbol=symbol, interval=timeframe, limit=period)
+        converted_data = []
 
-# Получение данных свечей по инструменту
-def get_candlestick_data(symbol, timeframe, period) -> list:
-    raw_data = Spot().klines(symbol=symbol, interval=timeframe, limit=period)
-    converted_data = []
+        for candle in raw_data:
+            converted_data.append({
+                "time": int(candle[0]),
+                "open": float(candle[1]),
+                "high": float(candle[2]),
+                "low": float(candle[3]),
+                "close": float(candle[4]),
+                "volume": float(candle[5]),
+                "close_time": candle[6],
+                "quote_asset_volume": float(candle[7]),
+                "number_of_trades": int(candle[8]),
+                "taker_buy_base_asset_volume": float(candle[9]),
+                "taker_buy_quote_asset_volume": float(candle[10])
+            })
 
-    for candle in raw_data:
-        converted_data.append({
-            "time": int(candle[0]),
-            "open": float(candle[1]),
-            "high": float(candle[2]),
-            "low": float(candle[3]),
-            "close": float(candle[4]),
-            "volume": float(candle[5]),
-            "close_time": candle[6],
-            "quote_asset_volume": float(candle[7]),
-            "number_of_trades": int(candle[8]),
-            "taker_buy_base_asset_volume": float(candle[9]),
-            "taker_buy_quote_asset_volume": float(candle[10])
-        })
+        return converted_data
 
-    return converted_data
-
-
-def make_trade_with_params(spot: Spot, **kwargs):
-    print("Making trade with params")
-    try:
-        response = spot.new_order(**kwargs)
-        return response
-    except ConnectionRefusedError as error:
-        _log.error(f"New order error: {error}")
+    def make_trade_with_params(self, **kwargs):
+        print("Making trade with params")
+        try:
+            response = self.spot.new_order(**kwargs)
+            return response
+        except ConnectionRefusedError as error:
+            _logger.error(f"New order error: {error}")
--- src/app.py
+++ src/app.py
@@ -1,16 +1,28 @@
 """Expert Application
 """
+import json
+import pprint
+
 from _logger import getLogger
 from flask import Flask, render_template, request, flash, redirect, jsonify
+from binance.spot import Spot
 
-logger = getLogger(__name__)
+_logger = getLogger(__name__)
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    title = "Home"
-    return render_template("view.home.tmpl", title=title)
+    spot = Spot()
+    infos = spot.exchange_info()
+    symbols = infos["symbols"]
+
+    props = {
+        "title": "Home",
+        "infos": infos,
+        "symbols": symbols
+    }
+    return render_template("view.home.tmpl", **props)
 
 
 @app.route("/about")
@@ -21,17 +33,140 @@ def about():
 
 @app.route("/hello")
 def hello():
-    return "<h1>Hello, World!</h1>"
+    """{
+    timezone: "UTC",
+    serverTime: 1633012903441,
+    rateLimits: [
+            {
+              "interval": "MINUTE",
+              "intervalNum": 1,
+              "limit": 6000,
+              "rateLimitType": "REQUEST_WEIGHT"
+            },
+            ...
+        ]
+    exchangeFilters: [],
+    symbols: [
+            {
+              "allowTrailingStop": true,
+              "allowedSelfTradePreventionModes": [
+                "EXPIRE_TAKER",
+                "EXPIRE_MAKER",
+                "EXPIRE_BOTH"
+              ],
+              "baseAsset": "ETH",
+              "baseAssetPrecision": 8,
+              "baseCommissionPrecision": 8,
+              "cancelReplaceAllowed": true,
+              "defaultSelfTradePreventionMode": "EXPIRE_MAKER",
+              "permissions": [],
+              "quoteAsset": "BTC",
+              "quoteAssetPrecision": 8,
+              "quoteCommissionPrecision": 8,
+              "quoteOrderQtyMarketAllowed": true,
+              "quotePrecision": 8,
+              "status": "TRADING",
+              "symbol": "ETHBTC",
+              "filters": [
+                {
+                  "filterType": "PRICE_FILTER",
+                  "maxPrice": "922327.00000000",
+                  "minPrice": "0.00001000",
+                  "tickSize": "0.00001000"
+                },
+                {
+                  "filterType": "LOT_SIZE",
+                  "maxQty": "100000.00000000",
+                  "minQty": "0.00010000",
+                  "stepSize": "0.00010000"
+                },
+                {
+                  "filterType": "ICEBERG_PARTS",
+                  "limit": 10
+                },
+                {
+                  "filterType": "MARKET_LOT_SIZE",
+                  "maxQty": "2077.27362125",
+                  "minQty": "0.00000000",
+                  "stepSize": "0.00000000"
+                },
+                {
+                  "filterType": "TRAILING_DELTA",
+                  "maxTrailingAboveDelta": 2000,
+                  "maxTrailingBelowDelta": 2000,
+                  "minTrailingAboveDelta": 10,
+                  "minTrailingBelowDelta": 10
+                },
+                {
+                  "askMultiplierDown": "0.2",
+                  "askMultiplierUp": "5",
+                  "avgPriceMins": 5,
+                  "bidMultiplierDown": "0.2",
+                  "bidMultiplierUp": "5",
+                  "filterType": "PERCENT_PRICE_BY_SIDE"
+                },
+                {
+                  "applyMaxToMarket": false,
+                  "applyMinToMarket": true,
+                  "avgPriceMins": 5,
+                  "filterType": "NOTIONAL",
+                  "maxNotional": "9000000.00000000",
+                  "minNotional": "0.00010000"
+                },
+                {
+                  "filterType": "MAX_NUM_ORDERS",
+                  "maxNumOrders": 200
+                },
+                {
+                  "filterType": "MAX_NUM_ALGO_ORDERS",
+                  "maxNumAlgoOrders": 5
+                }
+              ],
+              "icebergAllowed": true,
+              "isMarginTradingAllowed": true,
+              "isSpotTradingAllowed": true,
+              "ocoAllowed": true,
+              "orderTypes": [
+                "LIMIT",
+                "LIMIT_MAKER",
+                "MARKET",
+                "STOP_LOSS",
+                "STOP_LOSS_LIMIT",
+                "TAKE_PROFIT",
+                "TAKE_PROFIT_LIMIT"
+              ],
+              "otoAllowed": true,
+              "permissionSets": [
+                [
+                  "SPOT",
+                  "MARGIN",
+                  "TRD_GRP_004",
+                  "TRD_GRP_005",
+                  "TRD_GRP_006",
+                  ...
+                  "TRD_GRP_054"
+                ]
+              ]
+            },
+            ...
+        ]
+    }
+    """
+    spot = Spot()
+    infos = spot.exchange_info()
+
+    return jsonify(infos)
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('view.error.tmpl', error=error), 404
+    title = "404"
+    return render_template('view.error.tmpl', title=title, error=error), 404
 
 
 if __name__ == "__main__":
-    logger.info("start app")
+    _logger.info("start app")
 
     app.run(port=5001, debug=True)
 
-    logger.info("finish app")
+    _logger.info("finish app")
